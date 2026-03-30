5da7d3892e3e7ca22a1bd35a56dbb55ef0a75c30
viren <virenrangani7@gmail.com>
2026-03-30 17:00:30 +0530

    refactor : update the signup screen code

--- lib/feature/auth/presentation/pages/signup.dart
+++ lib/feature/auth/presentation/pages/signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foodygo/core/constant/image/app_image/app_image.dart';
 import 'package:foodygo/core/constant/string/custom_string.dart';
 import 'package:foodygo/core/widget/snack_bar/custom_snack_bar.dart';
 import 'package:foodygo/core/widget/text_form_field/custom_text_form_field.dart';
@@ -8,7 +9,6 @@ import '../../../../core/constant/color/custom_color.dart';
 import '../../../../core/constant/font_size/custom_text_style.dart';
 import '../../../../core/constant/padding/custom_padding.dart';
 import '../../../../core/widget/elevated_button/custom_elevated_button.dart';
-import '../../../../pages/bottomnav.dart';
 import '../cubit/auth_state.dart';
 import '../cubit/signup_cubit.dart';
 import 'login.dart';
@@ -21,10 +21,11 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
-  final nameController = TextEditingController();
+  final nameController  = TextEditingController();
   final emailController = TextEditingController();
-  final passController = TextEditingController();
+  final passController  = TextEditingController();
   final formKey = GlobalKey<FormState>();
+  bool _obscurePassword = true;
 
   @override
   void dispose() {
@@ -37,7 +38,7 @@ class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
+    final screenWidth  = MediaQuery.of(context).size.width;
     final isSmallScreen = screenWidth < 600;
 
     return BlocProvider(
@@ -46,16 +47,22 @@ class _SignupState extends State<Signup> {
         body: BlocConsumer<SignupCubit, AuthState>(
           listener: (context, state) {
             if (state is AuthSuccess) {
-              CustomSnacksBar.showSuccess(context, CustomString.signSuccess);
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context)=>BottomNav())
+              CustomSnacksBar.showSuccess(
+                context,
+                CustomString.signUpEmailVerify,
+              );
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (_) => const Login()),
+                    (_) => false,
               );
             } else if (state is AuthFailure) {
               CustomSnacksBar.showError(context, state.message);
             }
           },
           builder: (context, state) {
+            final isLoading = state is AuthLoading;
+
             return SingleChildScrollView(
               child: Stack(
                 children: [
@@ -77,7 +84,7 @@ class _SignupState extends State<Signup> {
                     decoration: const BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(50),
+                        topLeft:  Radius.circular(50),
                         topRight: Radius.circular(50),
                       ),
                     ),
@@ -88,7 +95,7 @@ class _SignupState extends State<Signup> {
                         margin: EdgeInsets.only(top: screenHeight * 0.1),
                         child: Center(
                           child: Image.asset(
-                            'assets/image/food.png',
+                            AppImage.foody,
                             color: Colors.black,
                             width: screenWidth / (isSmallScreen ? 2 : 3),
                             fit: BoxFit.cover,
@@ -103,7 +110,9 @@ class _SignupState extends State<Signup> {
                           borderRadius: BorderRadius.circular(30),
                           child: Container(
                             padding: EdgeInsets.symmetric(
-                                horizontal: isSmallScreen ? 20 : 30, vertical: 20),
+                              horizontal: isSmallScreen ? 20 : 30,
+                              vertical: 20,
+                            ),
                             width: screenWidth * 0.9,
                             decoration: BoxDecoration(
                               color: Colors.white,
@@ -117,56 +126,88 @@ class _SignupState extends State<Signup> {
                                   Text(
                                     CustomString.signUp,
                                     style: TextStyle(
-                                        fontSize: isSmallScreen ? 28 : 35,
-                                        fontWeight: FontWeight.bold),
+                                      fontSize: isSmallScreen ? 28 : 35,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
                                   SizedBox(height: screenHeight * 0.02),
+
                                   CustomFormField(
+                                    controller: nameController,
                                     prefixIcon: Icons.person_2_outlined,
-                                      labelText: CustomString.name,
-                                      hintText: CustomString.nameHint
-                                  ),
-                                  SizedBox(height: screenHeight * 0.015),
-                                  CustomFormField(
-                                    labelText: CustomString.email,
-                                    hintText: CustomString.emailHint,
-                                    prefixIcon: Icons.email_outlined,
-                                    validator: (val){
-                                      if(val==null||val.isEmpty){
-                                        return "Enter the mail";
-                                      }return null;
+                                    labelText: CustomString.name,
+                                    hintText: CustomString.nameHint,
+                                    validator: (val) {
+                                      if (val == null || val.trim().isEmpty) {
+                                        return 'Enter your name';
+                                      }
+                                      return null;
                                     },
                                   ),
                                   SizedBox(height: screenHeight * 0.015),
+
                                   CustomFormField(
+                                    controller: emailController,
+                                    labelText: CustomString.email,
+                                    hintText: CustomString.emailHint,
+                                    prefixIcon: Icons.email_outlined,
+                                    keyboardType: TextInputType.emailAddress,
+                                    validator: (val) {
+                                      if (val == null || val.isEmpty) {
+                                        return 'Enter your email';
+                                      }
+                                      if (!RegExp(r'^[\w-.]+@([\w-]+\.)+[\w]{2,4}$')
+                                          .hasMatch(val)) {
+                                        return 'Enter a valid email';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  SizedBox(height: screenHeight * 0.015),
+
+                                  CustomFormField(
+                                    controller: passController,
                                     labelText: CustomString.password,
                                     hintText: CustomString.passwordHint,
-                                    suffixIcon:Icons.remove_red_eye_outlined ,
+                                    obscureText: _obscurePassword,
                                     prefixIcon: Icons.password_outlined,
-                                    validator: (val){
-                                      if(val==null||val.isEmpty){
-                                        return "password is empty";
+                                    suffixIcon: _obscurePassword
+                                        ? Icons.remove_red_eye_outlined
+                                        : Icons.visibility_off_outlined,
+                                    onSuffixTap: () => setState(
+                                            () => _obscurePassword = !_obscurePassword),
+                                    validator: (val) {
+                                      if (val == null || val.isEmpty) {
+                                        return 'Password is empty';
+                                      }
+                                      if (val.length < 6) {
+                                        return 'Password must be at least 6 characters';
                                       }
                                       return null;
                                     },
                                   ),
                                   SizedBox(height: screenHeight * 0.02),
-                                  CustomElevatedButton(
-                                      text:CustomString.signUp,
-                                      color1: CustomColor.secondary,
-                                      color2: CustomColor.primary,
-                                      width:screenWidth*0.4,
-                                      height: screenHeight*0.05,
-                                      onPressed: (){
-                                        if (formKey.currentState!.validate()){
-                                          context.read<SignupCubit>()
-                                              .signUp(
-                                              email:emailController.text,
-                                              password: passController.text,
-                                              name: nameController.text
-                                          );
-                                        }
-                                      })
+
+                                  isLoading
+                                      ? const CircularProgressIndicator(
+                                    color: Colors.deepOrange,
+                                  )
+                                      : CustomElevatedButton(
+                                    text: CustomString.signUp,
+                                    color1: CustomColor.secondary,
+                                    color2: CustomColor.primary,
+                                    width: screenWidth * 0.4,
+                                    height: screenHeight * 0.05,
+                                    onPressed: () {
+                                      if (formKey.currentState!.validate()) {
+                                        context.read<SignupCubit>().signUp(
+                                          email: emailController.text.trim(),
+                                          password: passController.text,
+                                          name: nameController.text.trim(),
+                                        );
+                                      }
+                                    },
+                                  ),
                                 ],
                               ),
                             ),
@@ -185,17 +226,24 @@ class _SignupState extends State<Signup> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(CustomString.notAccount,style: CustomTextStyles.bodyMedium,),
-              InkWell(onTap: (){
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context)=>Login())
-                );
-              },
-                child: Text(CustomString.signUp ,style: CustomTextStyles.bodyLarge,),)
+              Text(
+                CustomString.alreadyHaveAccount,
+                style: CustomTextStyles.bodyMedium,
+              ),
+              InkWell(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const Login()),
+                ),
+                child: Text(
+                  CustomString.login,
+                  style: CustomTextStyles.bodyLarge,
+                ),
+              ),
             ],
           ),
-      ),)
+        ),
+      ),
     );
-
   }
 }
