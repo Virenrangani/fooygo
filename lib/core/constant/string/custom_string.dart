c28670cf74bb0d2500b72a915919fa1135e5079f
viren <virenrangani7@gmail.com>
2026-03-30 17:52:47 +0530

    feat :add the string in the class

--- lib/core/constant/string/custom_string.dart
+++ lib/core/constant/string/custom_string.dart
@@ -4,39 +4,28 @@ class CustomString {
   static const String emailRequired="Email is required";
   static const String email="E-mail";
   static const String emailHint="Enter Mail";
-
   static const String emailNotContainsSpace="Email should not contain spaces";
-
   static const String emailValid="Enter a valid email";
 
   static const String passwordRequired="Password is required";
   static const String password="Password";
   static const String passwordHint="Enter Password";
   static const String userNotVerify="user is not verified";
-
   static const String passwordNotContainsSpace="Password should not contain spaces";
-
   static const String passwordMinChar="Minimum 8 characters required";
-
   static const String atLeastOneUpperCase="Add at least one uppercase letter";
-
   static const String atLeastOneLowerCase="Add at least one lowercase letter";
-
   static const String atLeastOneNumber="Add at least one number";
-
   static const String atLeastOneSpecialChar="Add at least one special character";
 
   static const String login="Login";
-
   static const String loginSuccess="Login Successfully ";
   static const String logInIntro="Please Enter the Login details";
-
   static const String notAccount="Don't have an account??";
+  static const String googleSignInFailed='Google sign-in cancelled';
 
   static const String signInWithGoogle="Google";
-
-  static const String signInWithFacebook="Facebook";
-
+  static const String signInWithGithub="Github";
   static const String userNotFound="user is not found";
   static const String userNotCreated="User is not created";
 
@@ -47,60 +36,22 @@ class CustomString {
   static const String signSuccess="SignUp Successfully..!!";
   static const String alreadyHaveAccount="Already have an account?";
   static const String signUpEntro="Please Enter the SignUp details";
+  static const String signUpEmailVerify="Account created! Please verify your email before logging in";
 
   static const String name="Name";
   static const String nameHint="Enter Name";
+  static const String admin="Admin";
 
   static const String forgetPassword="Forget password?";
 
-  static const String flipkart ="flipkart";
-  static const String search ="Search";
-  static const String searchItem ="Search for products and more";
-
-  static const String forYou="For You";
-  static const String fashion="Fashion";
-  static const String mobile="Mobiles";
-  static const String beauty="Beauty";
-  static const String home="Home";
-  static const String appliances="Appliances";
-  static const String toy="Toys";
-  static const String food="Foods";
-  static const String sports="Sports";
-  static const String book="Books";
-  static const String furniture="Furniture";
-
-  static const String play="Play";
-  static const String category="Category";
-  static const String cart="Cart";
+  static const String letsSlide="Let's slide!";
+  static const String slideButtonForSignAdmin="Slide this button to admin or signup.";
+  static const String slidMe="Slide Me";
   static const String profile="Profile";
 
+  static const String getStarted="Get Started";
+  static const String skip="Skip";
+  static const String next="next";
 
-  static const String myCart="My Cart";
-  static const String added="added.!!";
-  static const String cartEmpty="Your cart is empty";
-  static const String cartAdded="Add items to get started";
-  static const String inCart="in your cart";
-  static const String subTotal="SubTotal";
-  static const String total="Total";
-  static const String free="Free";
-  static const String delivery="Delivery";
-  static const String checkOut="Proceed to Checkout";
-  static const String description="Description";
-  static const String addToCart="Add to Cart";
-
-
-  static const String editProfile="Edit Profile";
-  static const String addressProfile="Address Management";
-  static const String help="Help";
-  static const String settings="Settings";
-  static const String logOut="Log Out";
-
-  static const String viren="Viren Rangani";
-  static const String virenMail="virenrangani7@gmail.com";
-
-  static const String order="Order";
-  static const String voucher="Voucher";
-  static const String history="History";
-  static const String giftCoupons="gift & coupons";
 
 }
