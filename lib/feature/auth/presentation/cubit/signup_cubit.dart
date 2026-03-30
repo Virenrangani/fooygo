b0afabf078d9413bb5ee23734812677082533d8a
viren <virenrangani7@gmail.com>
2026-03-30 17:00:11 +0530

    refactor : update the code of the cubit for better state management

--- lib/feature/auth/presentation/cubit/signup_cubit.dart
+++ lib/feature/auth/presentation/cubit/signup_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:foodygo/feature/auth/domain/usecase/sign_up_use_case.dart';
+import '../../../../core/storage/shared_pref/shared_pref_service.dart';
 import 'auth_state.dart';
 
 class SignupCubit extends Cubit<AuthState> {
@@ -14,11 +15,17 @@ class SignupCubit extends Cubit<AuthState> {
   }) async {
     emit(AuthLoading());
     try {
-      await signUpUseCase.signUpCall(email, name, password);
+      final user = await signUpUseCase.signUpCall(email, name, password);
+
+      await SharedPrefService.saveUser(
+        id: user.id,
+        email: user.email,
+        name: user.name ?? '',
+      );
 
       emit(AuthSuccess());
     } on FirebaseAuthException catch (e) {
-      emit(AuthFailure(e.message ?? "Registration failed"));
+      emit(AuthFailure(e.toString()));
     } catch (e) {
       emit(AuthFailure(e.toString()));
     }
