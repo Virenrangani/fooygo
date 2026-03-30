0a54bf84430a0aa60788da166842c42dd9c7d61e
viren <virenrangani7@gmail.com>
2026-03-30 16:58:07 +0530

    feat : add the shared preference helper class for locally store the data

---
+++ lib/core/storage/shared_pref/shared_pref_service.dart
@@ -0,0 +1,45 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefService {
+  static const _keyUserId    = 'user_id';
+  static const _keyUserEmail = 'user_email';
+  static const _keyUserName  = 'user_name';
+  static const _keyIsLoggedIn = 'is_logged_in';
+
+  static Future<void> saveUser({
+    required String id,
+    required String email,
+    required String name,
+  }) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_keyUserId, id);
+    await prefs.setString(_keyUserEmail, email);
+    await prefs.setString(_keyUserName, name);
+    await prefs.setBool(_keyIsLoggedIn, true);
+  }
+
+  static Future<String?> getUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_keyUserId);
+  }
+
+  static Future<String?> getUserEmail() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_keyUserEmail);
+  }
+
+  static Future<String?> getUserName() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_keyUserName);
+  }
+
+  static Future<bool> isLoggedIn() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_keyIsLoggedIn) ?? false;
+  }
+
+  static Future<void> clearUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+  }
+}
