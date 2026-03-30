e11838fef3fc13667cabb9b6a3500454e645c2c6
viren <virenrangani7@gmail.com>
2026-03-30 16:59:03 +0530

    refactor : update the code of the data layer

--- lib/feature/auth/data/data_source/sign_up_data_source.dart
+++ lib/feature/auth/data/data_source/sign_up_data_source.dart
@@ -3,29 +3,31 @@ import '../../../../core/constant/string/custom_string.dart';
 import '../model/user_model.dart';
 
 abstract class SignUpDataSource {
-  Future<UserModel> signUp(String email,String name,String password);
+  Future<UserModel> signUp(String email, String name, String password);
 }
 
-class SignUpDataSourceImpl implements SignUpDataSource{
+class SignUpDataSourceImpl implements SignUpDataSource {
   final FirebaseAuth firebaseAuth;
   SignUpDataSourceImpl(this.firebaseAuth);
 
   @override
-  Future<UserModel> signUp(String email, String name, String password) async{
-    try{
-      UserCredential credential=
-          await FirebaseAuth.instance
-          .createUserWithEmailAndPassword(
-          email: email,
-          password: password
-      );
-      final user= credential.user;
+  Future<UserModel> signUp(String email, String name, String password) async {
+    try {
+      UserCredential credential = await firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
 
-      if (user!=null && !user.emailVerified){
-        await user.sendEmailVerification();
-      }
-      return UserModel.fromFirebase(user!);
-    }catch(e){
+      final user = credential.user;
+      if (user == null) throw Exception(CustomString.somethingWentWrong);
+
+      await user.updateDisplayName(name);
+      await user.reload();
+
+      await user.sendEmailVerification();
+
+      return UserModel.fromFirebase(firebaseAuth.currentUser!);
+    } on FirebaseAuthException {
+      rethrow;
+    } catch (e) {
       throw Exception(CustomString.somethingWentWrong);
     }
   }
--- lib/feature/auth/data/repository/sign_up_repository_impl.dart
+++ lib/feature/auth/data/repository/sign_up_repository_impl.dart
@@ -2,15 +2,13 @@ import '../../domain/entity/user_entity.dart';
 import '../../domain/repository/sign_up_repository.dart';
 import '../data_source/sign_up_data_source.dart';
 
-class SignUpRepositoryImpl implements SignUpRepository{
+class SignUpRepositoryImpl implements SignUpRepository {
   final SignUpDataSource signUpDataSource;
-
   SignUpRepositoryImpl(this.signUpDataSource);
 
   @override
-  Future<UserEntity> signUp(String email, String name, String password) async{
-    final model= await signUpDataSource.signUp(email, name, password);
-    return UserEntity(id: model.id, email: model.email,name: model.name);
+  Future<UserEntity> signUp(String email, String name, String password) async {
+    final model = await signUpDataSource.signUp(email, name, password);
+    return UserEntity(id: model.id, email: model.email, name: model.name);
   }
-
 }
