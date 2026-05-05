7606cc4cbfe5733d00ae5ac9cb2452b13f0b3f82
viren <virenrangani7@gmail.com>
2026-05-05 15:42:18 +0530

    refactor: remove unused border radius

--- lib/core/constant/border/custom_border_radius.dart
+++ lib/core/constant/border/custom_border_radius.dart
@@ -15,24 +15,4 @@ class CustomBorderRadius {
   static BorderRadius verTop16=BorderRadius.vertical(top:Radius.circular(16));
   static BorderRadius verTop20=BorderRadius.vertical(top:Radius.circular(20));
   static BorderRadius verTop24=BorderRadius.vertical(top: Radius.circular(24));
-
-  //symmetric vertical bottom
-  static BorderRadius verBottom12=BorderRadius.vertical(bottom:Radius.circular(12));
-  static BorderRadius verBottom16=BorderRadius.vertical(bottom:Radius.circular(16));
-  static BorderRadius verBottom20=BorderRadius.vertical(bottom:Radius.circular(20));
-
-
-  //symmetric horizontal left
-  static BorderRadius horiLeft12=BorderRadius.horizontal(left:Radius.circular(12));
-  static BorderRadius horiLeft16=BorderRadius.horizontal(left:Radius.circular(16));
-  static BorderRadius horiLeft20=BorderRadius.horizontal(left:Radius.circular(20));
-
-  //symmetric horizontal right
-  static BorderRadius horiRight12=BorderRadius.horizontal(left:Radius.circular(12));
-  static BorderRadius horiRight16=BorderRadius.horizontal(left:Radius.circular(16));
-  static BorderRadius horiRight20=BorderRadius.horizontal(left:Radius.circular(20));
-
-  static BorderRadius onlyTopBottomRight12=BorderRadius.only(
-    topRight: Radius.circular(12), bottomRight: Radius.circular(12),);
-
 }
