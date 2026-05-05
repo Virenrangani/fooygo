78f68e27c533ede6fec2dfb6c65d9d55ae398a21
viren <virenrangani7@gmail.com>
2026-05-05 16:14:26 +0530

    refactor: remove unused widget and add color , padding

--- lib/feature/onboard_screen/slide_button_screen/slide_button_page.dart
+++ lib/feature/onboard_screen/slide_button_screen/slide_button_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:foodygo/core/constant/color/custom_color.dart';
+import 'package:foodygo/core/constant/font_size/custom_text_style.dart';
+import 'package:foodygo/core/constant/padding/custom_padding.dart';
 import 'package:foodygo/core/constant/string/custom_string.dart';
 import 'package:foodygo/feature/auth/presentation/pages/login.dart';
 import 'package:foodygo/feature/auth/presentation/pages/signup.dart';
@@ -17,64 +20,60 @@ class _ClickableButtonState extends State<ClickableButton> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration:BoxDecoration(
-          gradient:LinearGradient(colors:[Colors.
-              deepOrange,Colors.deepOrangeAccent],
-            begin:Alignment.topLeft,end:Alignment.bottomRight
-          )
-        ),
-        child: Center(
+      backgroundColor: CustomColor.primaryLight,
+      body: Center(
+        child: Expanded(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                     Flexible(
-                       child: Padding(
-                         padding: const EdgeInsets.all(10),
-                         child: Text(CustomString.slideButtonForSignAdmin,
-                          style:TextStyle(fontSize:25,fontWeight:FontWeight.bold),),
-                       ),
-                     ),
-                    const SizedBox(height: 16.0),
-                    HorizontalSlidableButton(
-                      initialPosition: SlidableButtonPosition.center,
-                      width: MediaQuery.of(context).size.width / 1.5,
-                      height:60,
-                      buttonWidth: 80.0,
-                      color:Colors.black,
-                      buttonColor:Colors.white,
-                      dismissible: false,
-                      label: const Center(child: Text(CustomString.slidMe,overflow:TextOverflow.ellipsis,
-                        style:TextStyle(fontSize:18,fontWeight:FontWeight.bold,color: Colors.black),)),
-                      child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(CustomString.login,style:TextStyle(fontSize:15,fontWeight:FontWeight.bold,color: Colors.white),),
-                            Text(CustomString.admin,style:TextStyle(fontSize:15,fontWeight:FontWeight.bold,color: Colors.white),),
-                          ],
-                        ),
-                      ),
-                      onChanged: (position) {
-                        setState(() {
-                          if (position == SlidableButtonPosition.end) {
-                            Navigator.push(context,
-                                MaterialPageRoute(builder:(context) =>AdminLoginPage(),));
-                          } else {
-                            Navigator.push(context,
-                                MaterialPageRoute(builder:(context) =>Login(),));;
-                          }
-                        });
-                      },
-                    ),
-                  ],
+
+               Flexible(
+                 child: Padding(
+                   padding: CustomPadding.edgeAll12,
+                   child: Text(CustomString.slideButtonForSignAdmin,
+                    style:CustomTextStyles.h3),
+                 ),
+               ),
+
+              const SizedBox(height: 16.0),
+
+              HorizontalSlidableButton(
+                initialPosition: SlidableButtonPosition.center,
+                width: MediaQuery.of(context).size.width / 1.5,
+                height:60,
+                buttonWidth: 80.0,
+                color:CustomColor.textPrimary,
+                buttonColor:CustomColor.background,
+                dismissible: false,
+                label: const Center(child: Text(CustomString.slidMe,overflow:TextOverflow.ellipsis,
+                  style:CustomTextStyles.bodyLarge
+                )
                 ),
+                child: Padding(
+                  padding: CustomPadding.edgeAll8,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+
+                      Text(CustomString.login,
+                        style:CustomTextStyles.h3.copyWith(color:CustomColor.background),),
+
+                      Text(CustomString.admin,
+                        style:CustomTextStyles.h3.copyWith(color:CustomColor.background),),
+                    ],
+                  ),
+                ),
+                onChanged: (position) {
+                  setState(() {
+                    if (position == SlidableButtonPosition.end) {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder:(context) =>AdminLoginPage(),));
+                    } else {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder:(context) =>Login(),));;
+                    }
+                  });
+                },
               ),
             ],
           ),
