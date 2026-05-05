6bddfe58ade49a49449ca40c42645c4e2e9686e1
viren <virenrangani7@gmail.com>
2026-05-05 15:42:53 +0530

    feat: add onBoarding image

--- lib/core/constant/image/app_image/app_image.dart
+++ lib/core/constant/image/app_image/app_image.dart
@@ -1,3 +1,8 @@
 class AppImage {
   static const String foody="assets/image/food.png";
+
+  static const String onBoarding1="assets/image/screen1.png";
+  static const String onBoarding2="assets/image/screen2.png";
+  static const String onBoarding3="assets/image/screen3.png";
+
 }
